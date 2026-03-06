74504c97d7eb6661f360675ef71a4a4454c9771a
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2026-03-06 19:23:33 +0500

    Device Owner Repository

---
+++ app/src/main/java/com/example/securityapp/modules/controlled/data/repository/DeviceOwnerRepository.kt
@@ -0,0 +1,62 @@
+package com.example.securityapp.modules.controlled.data.repository
+
+import android.app.PendingIntent
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import com.example.securityapp.MyDeviceAdminReceiver
+import com.example.securityapp.utils.Result
+import javax.inject.Inject
+
+class DeviceOwnerRepository @Inject constructor() {
+    fun resetPhone(context : Context): Result<Unit> {
+        return try {
+            val dpm = context.getSystemService(DevicePolicyManager::class.java)
+            val isOwner = dpm.isDeviceOwnerApp("com.example.securityapp")
+            when(isOwner){
+                true -> {
+                    dpm.setLockTaskPackages(
+                        ComponentName(context, MyDeviceAdminReceiver::class.java),
+                        arrayOf("com.example.securityapp")
+                    )
+                    dpm.wipeData(0)
+                    Result.Success(Unit)
+                }
+                false -> Result.Error("Error Resetting Phone, App is not Device Owner")
+            }
+        }
+        catch (e : Exception){
+            Result.Error("Error Resetting Phone")
+        }
+    }
+
+    fun deleteApp(context: Context){
+        val dpm = context.getSystemService(DevicePolicyManager::class.java)
+        val comp = ComponentName(context, MyDeviceAdminReceiver::class.java)
+        dpm.clearPackagePersistentPreferredActivities(comp, "com.target.app")
+        val intent = Intent(Intent.ACTION_DELETE)
+        intent.data = Uri.parse("package:com.target.app")
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context.startActivity(intent)
+    }
+    fun uninstallPackage(context: Context, packageName: String): Result<Unit> {
+        return try {
+            val CODE_UNINSTALL_RESULT = 1235
+            val ACTION_UNINSTALL_RESULT = "eu.sisik.removehideaps.ACTION_UNINSTALL_RESULT"
+            Log.d("KHAN","IN UNINSTALL")
+            val intentSender = PendingIntent.getBroadcast(context,
+                CODE_UNINSTALL_RESULT,
+                Intent(ACTION_UNINSTALL_RESULT),
+                PendingIntent.FLAG_IMMUTABLE).intentSender
+            val pi = context.packageManager.packageInstaller
+            pi.uninstall(packageName, intentSender)
+            Result.Success(Unit)
+        }
+        catch (e : Exception){
+            Result.Error("Error Deleting App")
+        }
+    }
+}
